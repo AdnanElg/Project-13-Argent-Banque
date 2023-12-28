00c3e49d4f78505899289fff08e330597c408b34
El Ghalbzouri Adnan <elghalbzouriadnan@gmail.com>
2023-12-28 15:43:27 +0100

    fix

--- back/server/database/connection.js
+++ back/server/database/connection.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
-const dotEnv = require('dotenv');
+const mongoose = require("mongoose");
+const dotEnv = require("dotenv");
 
 dotEnv.config();
 
 const databaseUrl =
-  process.env.DATABASE_URL || `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`
+  process.env.DATABASE_URL ||
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true });
+    console.log("Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -66,7 +66,6 @@ const Navbar = (): JSX.Element => {
         const handlePopstate = () => {
           dispatch(setIsLogin(false));
           dispatch(setToken(null));
-          localStorage.removeItem("TOKEN");
           navigate("/");
         };
 
